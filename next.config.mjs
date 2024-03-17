f8835b5bfeb700ea061b2c65a4dd3985155e1271
RATIU5 <jcmemmott20@outlook.com>
2024-03-17 12:32:32 -0600

    add: million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const config = {
   eslint: {
@@ -6,24 +7,21 @@ const config = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: process.env.BIGCOMMERCE_CDN_HOSTNAME ?? '*.bigcommerce.com',
-        port: '',
-        pathname: '/**/*',
-      }
-    ]
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: process.env.BIGCOMMERCE_CDN_HOSTNAME ?? '*.bigcommerce.com',
+      port: '',
+      pathname: '/**/*'
+    }]
   },
   async redirects() {
-    return [
-      {
-        source: '/password',
-        destination: '/',
-        permanent: true
-      }
-    ];
+    return [{
+      source: '/password',
+      destination: '/',
+      permanent: true
+    }];
   }
 };
-
-export default config;
+export default MillionLint.next({
+  rsc: true
+})(config);
