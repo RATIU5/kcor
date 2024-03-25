7b7343717b61e9ccfe647534ff37d48c352b7459
Jordan Zappitello <jordan.zappitello@maloufcompanies.com>
2024-03-25 11:38:57 -0600

    about page tablet styles done

--- next/app/about/page.tsx
+++ next/app/about/page.tsx
@@ -2,6 +2,7 @@ import AboutGrid from '@/components/about/grid';
 import AboutHero from '@/components/about/hero';
 import Logos from '@/components/about/logos';
 import Cards from '@/components/home/cards';
+import CTA from '@/components/home/cta';
 import Section from '@/components/ui/section';
 
 export default function Page() {
@@ -15,6 +16,9 @@ export default function Page() {
       </Section>
       <Section className="gap-16 bg-neutral-100 px-4 py-10 text-neutral-900 md:py-14 2xl:py-20">
         <AboutGrid />
+      </Section>
+      <Section className="gap-16 bg-stone-300 px-4 py-10 md:px-14 2xl:px-40">
+        <CTA />
       </Section>
       <Section className="gap-16 bg-neutral-100 px-4 py-10 text-neutral-100 md:py-14 2xl:py-20">
         <Cards />
