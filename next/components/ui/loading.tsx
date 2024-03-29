f0c96dc7a34eee5563bfcb157e1583671adb0bff
RATIU5 <jcmemmott20@outlook.com>
2024-03-29 14:18:36 -0600

    reset: to next project bootstrapped with t3 app

--- next/components/ui/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { IconSpinner } from '@/components/icons';
-
-export default function Loading() {
-  return (
-    <div className="flex h-full w-full flex-col items-center justify-center">
-      <IconSpinner className="h-8 w-8 animate-spin" />
-    </div>
-  );
-}
--- next/lib/bigcommerce/constants.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '@/lib/env';
-
-export const BIGCOMMERCE_API_URL =
-  env.BIGCOMMERCE_API_URL ?? 'https://api.bigcommerce.com';
-export const BIGCOMMERCE_CANONICAL_STORE_DOMAIN =
-  env.BIGCOMMERCE_CANONICAL_STORE_DOMAIN ?? 'mybigcommerce.com';
-export const BIGCOMMERCE_STORE_HASH = env.BIGCOMMERCE_STORE_HASH ?? 'undefined';
-export const BIGCOMMERCE_V3_API_ENDPOINT = `${BIGCOMMERCE_API_URL}/stores/${BIGCOMMERCE_STORE_HASH}/v3`;
-export const BIGCOMMERCE_V2_API_ENDPOINT = `${BIGCOMMERCE_API_URL}/stores/${BIGCOMMERCE_STORE_HASH}/v2`;
